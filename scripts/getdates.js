d6f26f7feb3fe06a9c0f29c421922bcdd3391f1e
eric-hogan <hog.eric@hotmail.com>
2025-01-11 17:58:05 -0700

    homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentyear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentyear
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified
