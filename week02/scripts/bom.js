1c8015c599174790ddec7139b98bbde162245916
eric-hogan <hog.eric@hotmail.com>
2025-05-17 11:36:32 -0600

    week 2 learning activities completed.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,20 +3,30 @@ const button = document.querySelector ('button');
 const list = document.querySelector('#list')
 
 
-list.append(li);
+
 
 button.addEventListener ('click', function() {
-    if (input.value.trim() !== '') {
+    if (input.value.trim() !== '') { 
+        // create elements the list 
         const li = document.createElement('li');
-        const delete_button = document.createElement('button');
+        const deleteButton = document.createElement('button');
 
-        li.textContent = input.value
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
 
+        li.appendChild(deleteButton);
         list.appendChild(li);
+        input.value = "";
+        
+        deleteButton.addEventListener ('click', function() {
+            list.removeChild(li);
+        });
 
-    else 
-    {input.focus};
+        input.focus()
+
+    } else { 
+        console.log("ivalid input please try again");
+        input.focus()}
 });
-        delete_button.textContent = '❌';
-        li.append(delete_button);};
-    
+        
+        
